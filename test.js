5dfa9a72336b83e0a95cd770d2ef88d2f05d7932
Darky <144300989+deadsec-darky@users.noreply.github.com>
2024-01-16 16:40:23 +0000

    fix2

--- test.js
+++ test.js
@@ -15,9 +15,7 @@ console.log('hekkow ef sd fds f')
 console.log('hekkow ef sd fds f')
 console.log('hekkow ef sd fds f')
 console.log('hekko ef sd fds f')
-console.log('hekkow ef sd fds f')
-console.log('hekkow ef sd fds f')
-console.log('hekkow ef sd fds f')
+
 
 
 console.log('hekkow ef sd fds f')
